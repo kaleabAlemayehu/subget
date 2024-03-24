a8cfcadc5da740a1fc1ffb5fa63821d791a50d5e
Neo AKA ( Kaleab Alemayehu ) <cipherlofi04@gmail.com>
2024-03-25 01:01:10 +0300

    adding colorful rendering

--- render.js
+++ render.js
@@ -1,8 +1,10 @@
 import chalk from "chalk";
 const log = console.log;
 const renderResult = (result) => {
-  
-  log(chalk.blue(result));
+  result.forEach(el => {
+    log(chalk.green.bgGray(` ${el.movieName}`,chalk.blue.bgGray(`- ${el.release}`, chalk.red.bgGray(`${el.language}`)
+    )));
+  });
 };
 
 export {
