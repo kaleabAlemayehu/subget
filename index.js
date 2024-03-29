5963ed8d3b8cd755d964b828b341ad8a4d5a6697
Neo AKA ( Kaleab Alemayehu ) <cipherlofi@gmail.com>
2024-03-28 20:55:10 -0400

    implimenting page navigation functionality and subtitle downloading functionality

--- index.js
+++ index.js
@@ -3,10 +3,68 @@ import { getSubs, downloadSubs } from "./request.js";
 
 // file process.argv[2];
 // register user
+let page = 1;
+let pointer = 0;
+const pages = [];
+const onPress = (ch, key) => {
+  console.clear();
+  if (key && key.ctrl && key.name == "c") {
+    process.stdin.pause();
+  }
+  switch (key.name) {
+    case "down": {
+      if (pointer >= 6) {
+        pointer = pointer;
+      } else {
+        pointer++;
+      }
+      break;
+    }
 
+    case "up": {
+      if (pointer < 1) {
+        pointer = pointer;
+      } else {
+        pointer--;
+      }
+      break;
+    }
+    case "return": {
+      if (pointer == 5) {
+        page = prevPage(page);
+      } else if (pointer == 6) {
+        page = nextPage(page);
+      } else {
+        downloadSubs(pages[page - 1][pointer]);
+      }
+      break;
+    }
+    default: {
+      pointer = pointer;
+      break;
+    }
+  }
+  renderResult(pages[page - 1], page, pointer);
+};
+const nextPage = (page) => {
+  if (page >= pages.length) {
+    page = page;
+  } else {
+    page++;
+  }
+  console.log(`the page is : ${page}`);
+  return page;
+};
+
+const prevPage = (page) => {
+  if (page <= 1) {
+    page = page;
+  } else {
+    page--;
+  }
+  return page;
+};
 const pagenation = (listOfSubs) => {
-  const pages = [];
-
   for (let i = 0; i < Math.floor(listOfSubs.length / 5); i++) {
     pages.push(listOfSubs.slice(i * 5, i * 5 + 5));
   }
@@ -15,44 +73,13 @@ const pagenation = (listOfSubs) => {
 
 const ctrl = async () => {
   try {
-    let page = 0;
-    let pointer = 0;
     const listOfSubs = await getSubs(
       "The Matrix (1999) 1080p BrRip x264 - 1.85GB - YIFY"
     );
     const pages = pagenation(listOfSubs);
-    renderResult(pages[page], page, pointer);
+    renderResult(pages[page - 1], page, pointer);
 
-    process.stdin.on("keypress", function (ch, key) {
-      console.clear();
-      if (key && key.ctrl && key.name == "c") {
-        process.stdin.pause();
-      }
-      switch (key.name) {
-        case "down": {
-          if (pointer > 6) {
-            pointer = pointer;
-          } else {
-            pointer++;
-          }
-          break;
-        }
-
-        case "up": {
-          if (pointer < 1) {
-            pointer = pointer;
-          } else {
-            pointer--;
-          }
-          break;
-        }
-        default: {
-          pointer = pointer;
-          break;
-        }
-      }
-      renderResult(pages[page], page, pointer);
-    });
+    process.stdin.on("keypress", onPress);
   } catch (error) {
     console.error(error);
   }
--- request.js
+++ request.js
@@ -1,40 +1,48 @@
-import {os} from "./configSetups.js";
+import { os } from "./configSetups.js";
 import Subtitles from "./Model.js";
-
+import https from "https";
+import fs from "fs";
 
 const getSubs = async (title) => {
   try {
-    const response =  await os.subtitles({
+    const response = await os.subtitles({
       query: title,
-    })
+    });
 
-    if(!response || !response.data ){
+    if (!response || !response.data) {
       throw new Error(`Invaild response`);
     }
     const listOfSubs = [];
-    response.data.forEach(el=> {
-    listOfSubs.push(new Subtitles(el.id, el.attributes.language, el.attributes.feature_details.movie_name, el.attributes.release));
-  });
+    response.data.forEach((el) => {
+      listOfSubs.push(
+        new Subtitles(
+          el.id,
+          el.attributes.language,
+          el.attributes.feature_details.movie_name,
+          el.attributes.release
+        )
+      );
+    });
 
-  return listOfSubs;
-
+    return listOfSubs;
   } catch (error) {
     console.error("error : ", error);
-    return["this sucks"];
+    return ["this sucks"];
   }
-
-
-
 };
 
-const downloadSubs = async (id) => {
+const downloadSubs = async (sub) => {
   const response = await os.download({
-    file_id: id,
+    file_id: sub.id,
   });
-  console.log(response);
+  const file = fs.createWriteStream(response.file_name);
+  const request = https.get(response.link, (res) => {
+    res.pipe(file);
+    file.on("finish", () => {
+      file.close();
+      console.log("Downloaded successfully");
+    });
+  });
 };
-export {
-  getSubs,
-  downloadSubs
 
-}
+export { getSubs, downloadSubs };
